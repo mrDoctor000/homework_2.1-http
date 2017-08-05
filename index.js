eb4247e8bbbca7c67da8bff51230bf11738bb392
Egor Ponomarev <egor@ponomarev.pp.ru>
2017-08-05 21:43:53 +0300

    service

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require('http');
 
 
 let data = JSON.stringify({
-  'firstname': 'Thomas',
   'lastName': 'Edison'
 });
 
 let options = {
+  host: 'netology.tomilomark.ru',
   port: 80,
-  path: 'http://netology.tomilomark.ru/api/v1/hash',
+  path: '/api/v1/hash',
   method: 'POST',
   headers: {
     'firstname': 'Thomas',
@@ -20,11 +20,13 @@ let request = http.request(options);
 request.write(data);
 request.on('error', err => console.error(err));
 request.on('response', response => {
-  let answer;
-  response.on('data', chunk => answer += chunk);
+  let answer = '';
+  response.on('data', function(chunk) {
+    return answer += chunk;
+  });
   response.on('end', () => {
     const data = JSON.parse(answer);
-    console.log(data);
+    console.log(`Ваш уникальный код: ${data.hash}`);
   });
 });
 
